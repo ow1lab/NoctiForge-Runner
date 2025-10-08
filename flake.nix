0f4674ae082d1777b586f5c021ad9b1aa6d427a3
Ow1Dev <31339803+Ow1Dev@users.noreply.github.com>
2025-10-08 19:24:33 +0200

    feat: new good flake file yes

--- flake.nix
+++ flake.nix
@@ -1,29 +1,33 @@
 {
-  description = "Simple flake with just devShell";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, rust-overlay }:
-    let
-      system = "x86_64-linux";
-      overlays = [ (import rust-overlay) ];
-      pkgs = import nixpkgs { inherit system overlays; };
-    in {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          protobuf
-          just
-          rust-analyzer
-          rustfmt
-
-          # Rust with both musl + gnu targets
-          (pkgs.rust-bin.stable.latest.default.override {
-            targets = [ "x86_64-unknown-linux-musl" "x86_64-unknown-linux-gnu" ];
-          })
-        ];
-      };
-    };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ rust-overlay.overlays.default ];
+        pkgs = import nixpkgs { inherit overlays system; };
+        rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            rust
+          ];
+        };
+      }
+    );
 }
